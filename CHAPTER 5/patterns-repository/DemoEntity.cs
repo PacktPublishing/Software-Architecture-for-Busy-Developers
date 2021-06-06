7a9433ca7c9f4cb7b997b3f339667426e30100b1
Stephane Eyskens <stephaneey@hotmail.com>
2021-06-06 15:23:10 +0200

    Add files via upload

---
+++ CHAPTER 5/patterns-repository/DemoEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace patterns_repository
+{
+    public class DemoEntity
+    {
+        public int id { get; set; }
+        public string property1 { get; set; }
+        public int property2 { get; set; }
+
+        
+    }
+}
