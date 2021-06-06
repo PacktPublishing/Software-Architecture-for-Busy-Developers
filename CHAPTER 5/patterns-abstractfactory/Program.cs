9215282234b9f9c3cd4a89a7385975d83dfe12a7
Stephane Eyskens <stephaneey@hotmail.com>
2021-06-06 15:21:11 +0200

    Add files via upload

---
+++ CHAPTER 5/patterns-abstractfactory/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Threading.Tasks;
+
+namespace patterns_factorymethod
+{
+    class Program
+    {
+        
+       
+        static void Main(string[] args){            
+            Console.Write("Bus (1) or Event Grid (2): ");
+            var choice = Console.ReadLine().Trim();
+            switch(choice){
+                case "1":
+                    Console.WriteLine(new BrokerMessageFactory().GetPublisher().ProviderName);
+                    break;
+                case "2":
+                    Console.WriteLine(new EvGridMessageFactory().GetPublisher().ProviderName);
+                    break;
+                default:
+                    Console.WriteLine("Oops, invalid choice");
+                    break;                
+            }            
+        }
+    }
+}
